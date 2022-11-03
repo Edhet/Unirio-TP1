7930cdf04e65382f717db8860d703dbe603774df
Eduardo P <edtvports@gmail.com>
2022-11-02 22:38:13 -0300

    Better performance

--- list-3/41.cpp
+++ list-3/41.cpp
@@ -3,6 +3,7 @@
 int main()
 {
     const int BASE_YEAR = 1600;
+    const int LEAP_Y_DAYS = 366, NORM_Y_DAYS = 365;
     int year, month, day;
     int month_sizes[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     int days_until_date = 0;
@@ -24,9 +25,11 @@ int main()
 
         if (iter_year != year)
         {
-            for (int i = 0; i < 12; i++)
-            {
-                days_until_date += month_sizes[i];
+            if (month_sizes[1] == 29) {
+                days_until_date += LEAP_Y_DAYS;
+            }
+            else {
+                days_until_date += NORM_Y_DAYS;
             }
         }
         else
@@ -41,7 +44,7 @@ int main()
                     {
                         days_until_date += month_sizes[i];
                     }
-                    days_until_date += day;
+                    days_until_date += day - 1;
                     printf(" Day number %d from 1/1/1600.\n", days_until_date);
                 }
                 else
@@ -57,35 +60,31 @@ int main()
         }
         month_sizes[1] = 28;
     }
-    days_until_date--;
     
-    if (days_until_date % 7 == 0)
+    int day_of_week = days_until_date % 7;
+    switch (day_of_week)
     {
-        printf("\n saturday.\n");
-    }
-    else if (days_until_date % 7 == 1)
-    {
-        printf("\n sunday.\n");
-    }
-    else if (days_until_date % 7 == 2)
-    {
-        printf("\n monday.\n");
-    }
-    else if (days_until_date % 7 == 3)
-    {
-        printf("\n tuesday.\n");
-    }
-    else if (days_until_date % 7 == 4)
-    {
-        printf("\n wednesday.\n");
-    }
-    else if (days_until_date % 7 == 5)
-    {
-        printf("\n thursday.\n");
-    }    
-    else if (days_until_date % 7 == 6)
-    {
-        printf("\n friday.\n");
+    case 0:
+        printf("\nSaturday\n");
+        break;
+    case 1:
+        printf("\nSunday\n");
+        break;
+    case 2:
+        printf("\nMonday\n");
+        break;
+    case 3:
+        printf("\nTuesday\n");
+        break;
+    case 4:
+        printf("\nWednesday\n");
+        break;
+    case 5:
+        printf("\nThursday\n");
+        break;
+    case 6:
+        printf("\nFriday\n");
+        break;
     }
     
     return 0;
